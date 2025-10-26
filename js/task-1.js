5d005e855c7357b7ff7be1167b4b1c1181f458b9
Чайка <user@MacBook-Pro-user.local>
2025-10-26 20:21:07 +0200

    Initial commit, add task-3

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,9 @@ function makeTransaction(quantity, pricePerDroid) {
   const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   return message;
 }
-const functionResult = makeTransaction();
+
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
+const result = makeTransaction(5, 3000);
+console.log(result);
